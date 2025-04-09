83b718cffa9c57526facb1c6e1e675a02412c746
kushagra200112 <56234880+kushagra200112@users.noreply.github.com>
2025-04-09 12:11:03 +1000

    Update Dimy.py
    Till Taask3, having some errors in usage of Shamir module

--- Dimy.py
+++ Dimy.py
@@ -1,9 +1,13 @@
-// Front-end program code
 import sys
 import secrets
 import time
-from cryptography.hazmat.primitives.secret_sharing import Shamir
+from Crypto.Protocol.SecretSharing import Shamir
 import socket
+
+UDP_IP='127.0.0.1'
+UDP_PORT=5005
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1) # Enable broadcast
 
 allowed_time = [15, 18, 21, 24, 27, 30]
 time_t=int(sys.argv[1])
@@ -32,6 +36,13 @@ def share_secret(secret, k, n):
         print(f"Share {i+1}: {share.hex()}")
     return share_split
 
+def broadcast_shares(share_split):
+    for i, share in enumerate(share_split):
+        sock.sendto(share, (UDP_IP, UDP_PORT))
+        print(f"Broadcasting share {i+1}: {share.hex()}")
+        time.sleep(3)  # Wait for 3 seconds before sending the next share
+    print("All shares broadcasted successfully.")
+    
 for current_time in allowed_time:
     ephid(current_time)
     if time_t<15 or time_t>30:
