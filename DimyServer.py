feb8d301a6b72f8d6b5d4d590ead3a2ba7179377
Sean <z5425828@ad.unsw.edu.au>
2025-04-17 16:37:04 +1000

    added a stub backend

--- DimyServer.py
+++ DimyServer.py
@@ -1,1 +1,59 @@
 # Backend server code
+import socket
+import threading
+
+IP = socket.gethostbyname(socket.gethostname())
+PORT = 55000
+ADDR = (IP, PORT)
+SIZE = 100
+FORMAT = "utf-8"
+DISCONNECT_MSG = "!DISCONNECTING"
+
+# Datastores for CBFs and QBFs
+CBF={}
+QBF={}
+
+# Compares the given QBF with the CBFs to see if there has been any issues
+def compareBloomFilters():
+    return
+
+
+def handleClient(conn, addr):
+    print(f"[NEW CONNECTION] {addr} connected.")
+
+    connected = True
+    while connected:
+        # Receive messages from the connection and decode it using our specified format
+        msg = conn.recv(SIZE).decode(FORMAT)
+        # Exit the while loop when we receive a message to disconnect
+        if msg == DISCONNECT_MSG:
+            connected = False
+        
+        print(f"[{addr}] {msg}")
+        # Create a new message, encode it in utf-8 and then send it.
+        msg = f"Message receive: {msg}"
+        conn.send(msg.encode(FORMAT))
+
+    # Finally close the connection
+    conn.close()
+
+def main():
+    print("[STARTING] Server is starting up...")
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind(ADDR)
+    
+    server.listen()
+    print(f"[LISTENING] Server is listening on {IP}:{PORT}.")
+
+    while True:
+        # Accept a client
+        conn, addr = server.accept()
+        # Create a new thread to handle actions with the newly accepted client
+        thread = threading.Thread(target=handleClient, args=(conn, addr))
+        thread.start()
+        print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}.")
+
+
+
+if __name__ == "__main__":
+    main()
