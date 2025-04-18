bc42160851b4bc16dc4827ae6f75660836df1f9b
Sean <z5425828@ad.unsw.edu.au>
2025-04-18 17:27:49 +1000

    changing ports and ips

--- Dimy.py
+++ Dimy.py
@@ -1,16 +1,23 @@
-<<<<<<< HEAD
 # Front-end program code
-=======
 import sys
 import secrets
 import time
 from Crypto.Protocol.SecretSharing import Shamir
 import socket
 
-UDP_IP='127.0.0.1'
-UDP_PORT=5005
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP
-sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1) # Enable broadcast
+# Server details
+SERVER_IP = socket.gethostbyname(socket.gethostname())
+SERVER_PORT = 55000
+SERVER_ADDR = (SERVER_IP, SERVER_PORT)
+
+# UDP port to listen to
+# Our Client and Server are both running on the same machine,
+# So the IP will be the same
+UDP_IP = SERVER_IP
+UDP_PORT = 50001
+
+
+
 
 allowed_time = [15, 18, 21, 24, 27, 30]
 time_t=int(sys.argv[1])
@@ -20,7 +27,8 @@ if k <3 and n<5 and k>n:
     print("Invalid values for k and n... Exiting")
     sys.exit()
 
-g = 5  # generator
+# Generator
+g = 5  
 
 def ephid(current_time):
     if current_time not in allowed_time:
@@ -33,17 +41,19 @@ def ephid(current_time):
     return eph_id
 
 def share_secret(secret, k, n):
-    share_split=Shamir.split(k,n,secret) # secrets is being splitted into n shares and being reconstructed using k shares
+    # secrets is being splited into n shares and being reconstructed using k shares
+    share_split=Shamir.split(k,n,secret) 
     print("Secret shares:")
     for i,share in enumerate(share_split):
         print(f"Share {i+1}: {share.hex()}")
     return share_split
 
-def broadcast_shares(share_split):
+def broadcast_shares(sock, share_split):
     for i, share in enumerate(share_split):
         sock.sendto(share, (UDP_IP, UDP_PORT))
         print(f"Broadcasting share {i+1}: {share.hex()}")
-        time.sleep(3)  # Wait for 3 seconds before sending the next share
+        # Wait for 3 seconds before sending the next share
+        time.sleep(3)
     print("All shares broadcasted successfully.")
     
 for current_time in allowed_time:
@@ -54,4 +64,27 @@ for current_time in allowed_time:
     share_secret(ephid(current_time), k, n)
     time.sleep(time_t) 
     
->>>>>>> 1ddef6776646e2edff865de2939e3c9e1cdf968f
+
+def main():
+    print("[CLIENT {PORT}] Client starting...")
+        
+    # Enable broadcasting with UDP
+    client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    client.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+    client.bind(('', 50001))
+
+    client.listen()
+    print(f"[BROADCASTING] Client is broadcasting through port {UDP_PORT}.")
+
+    while True:
+        # # Connect to server
+        # conn, addr = server.accept()
+        # # Create a new thread to handle actions with the newly accepted client
+        # thread = threading.Thread(target=handleClient, args=(conn, addr))
+        # thread.start()
+        # print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}.")
+        
+
+
+if __name__ == "__main__":
+    main()
